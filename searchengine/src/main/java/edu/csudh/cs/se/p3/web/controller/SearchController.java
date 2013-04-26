4b52c008f24a5e734cd9e69871fdab34bf6e8b84
amavileti <amavileti1@toromail.csudh.edu>
2013-04-26 12:02:10 -0700

    Added additional dependencies, need to configure DB, code, and other stuff

---
+++ searchengine/src/main/java/edu/csudh/cs/se/p3/web/controller/SearchController.java
@@ -0,0 +1,24 @@
+package edu.csudh.cs.se.p3.web.controller;
+
+
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.common.collect.Lists;
+
+@Controller
+@RequestMapping(method=RequestMethod.GET)
+public class SearchController {
+
+    
+    @ResponseBody
+    public List<String> search(@PathVariable String urlString){
+        return Lists.newArrayList();
+    }
+}
